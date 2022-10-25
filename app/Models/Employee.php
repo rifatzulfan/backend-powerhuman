5751a323440cbe86f9afed886a85735609c8d397
rifat <rifatzulfanmanaf@gmail.com>
2022-10-25 23:46:49 +0700

    first commit

---
+++ app/Models/Employee.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'gender',
+        'age',
+        'phone',
+        'photo',
+        'team_id',
+        'role_id',
+        'is_verified',
+        'verified_at',
+    ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+}
---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company(),
+            'logo' => $this->faker->imageUrl(),
+            'user_id' => $this->faker->numberBetween(1, 10)
+        ];
+    }
+}
